71d426fba378e36a424b68429e05206ec79ca0cc
Michel Flores <florespoblete17@gmail.com>
2023-12-13 21:45:09 -0300

    Idiomas y inicio, agregado de carpeta de sonido/Cambio agregado

---
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Entity/Enty.kt
@@ -0,0 +1,5 @@
+package com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Entity
+
+class Enty {
+
+}
---
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Listas/Lista_chistes.kt
@@ -0,0 +1,12 @@
+package com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Listas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flores_Alvarado_Sepulveda.chistesymaschistes.R
+
+class Lista_chistes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_chistes)
+    }
+}
