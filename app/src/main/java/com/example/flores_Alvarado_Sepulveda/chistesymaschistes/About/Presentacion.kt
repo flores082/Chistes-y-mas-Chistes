9a218280c903f280cdd0976d7cae6ef0e3a77030
Michel Flores <florespoblete17@gmail.com>
2023-12-13 22:25:34 -0300

    esqueleto 2

---
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/About/Presentacion.kt
@@ -0,0 +1,12 @@
+package com.example.flores_Alvarado_Sepulveda.chistesymaschistes.About
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.flores_Alvarado_Sepulveda.chistesymaschistes.R
+
+class Presentacion : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_presentacion)
+    }
+}
