3d4aac9ecef70938eb3a5dfee93885f45aace220
Michel Flores <florespoblete17@gmail.com>
2023-12-13 22:24:37 -0300

    esqueleto

---
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Adapter/Lista_Adapter.kt
@@ -0,0 +1,5 @@
+package com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Adapter
+
+class Lista_Adapter {
+
+}
--- app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Entity/Enty.kt
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Entity/Enty.kt
@@ -1,5 +1,13 @@
 package com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Entity
 
-class Enty {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serial
+import java.io.Serializable
 
-}
+@Entity(tableName = "nombres_table")
+data class ValoresEnty (
+   @PrimaryKey(autoGenerate = true)
+   val id : Long = 0,
+   val texto: String
+ ):Serializable
