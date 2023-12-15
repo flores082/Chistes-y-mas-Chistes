9681c1ed1a340b0bc34b583d473178a120923969
Michel Flores <florespoblete17@gmail.com>
2023-12-15 15:48:31 -0300

    vistas y arreglos

--- app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Listas/Llamar_chistes.kt
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/Listas/Llamar_chistes.kt
@@ -1,5 +1,6 @@
 package com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Listas
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -8,9 +9,12 @@ import android.widget.ImageButton
 import com.example.flores_Alvarado_Sepulveda.chistesymaschistes.R
 
 class Llamar_chistes : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_llamar_chistes)
+
+        mediaPlayer =MediaPlayer.create(this, R.raw.precionar_boton)
 
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.myToolbar)
         setSupportActionBar(toolbar)
@@ -18,6 +22,7 @@ class Llamar_chistes : AppCompatActivity() {
         // Configurar el botón de retorno (flecha)
         val btnBack: ImageButton = findViewById(R.id.btnBack)
         btnBack.setOnClickListener {
+            mediaPlayer.start()
             // Acciones al presionar la flecha de retorno
             onBackPressed()
         }
--- app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/MainActivity.kt
+++ app/src/main/java/com/example/flores_Alvarado_Sepulveda/chistesymaschistes/MainActivity.kt
@@ -1,16 +1,27 @@
 package com.example.flores_Alvarado_Sepulveda.chistesymaschistes
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.Toolbar
+import com.example.flores_Alvarado_Sepulveda.chistesymaschistes.About.Presentacion
+import com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Listas.Lista_chistes
+import com.example.flores_Alvarado_Sepulveda.chistesymaschistes.Listas.Llamar_chistes
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer =MediaPlayer.create(this, R.raw.precionar_boton)
 
         // Configurar el Toolbar
         val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.myToolbar)
@@ -18,9 +29,30 @@ class MainActivity : AppCompatActivity() {
 
         // Configurar el botón de retorno (flecha)
         val btnBack: ImageButton = findViewById(R.id.btnBack)
+        val presintacion: Button = findViewById(R.id.button1)
+        val generados: Button = findViewById(R.id.button2)
+        val guardados: Button = findViewById(R.id.button3)
+
         btnBack.setOnClickListener {
+            mediaPlayer.start()
             // Acciones al presionar la flecha de retorno
             finishAffinity()
+        }
+
+        presintacion.setOnClickListener{
+            mediaPlayer.start()
+            val intent1 =Intent(this, Presentacion::class.java)
+            startActivity(intent1)
+        }
+        generados.setOnClickListener{
+            mediaPlayer.start()
+            val intent2 =Intent(this, Llamar_chistes::class.java)
+            startActivity(intent2)
+        }
+        guardados.setOnClickListener{
+            mediaPlayer.start()
+            val intent3 =Intent(this, Lista_chistes::class.java)
+            startActivity(intent3)
         }
     }
 
